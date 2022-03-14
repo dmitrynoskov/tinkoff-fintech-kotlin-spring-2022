e73b956ff4f41f99a9032ec5ad9bbcdfcb84a21b
Dmitry Noskov <34381175+dmitrynoskov@users.noreply.github.com>
2022-03-14 22:39:09 +0300

    homework04 solution added

---
+++ src/test/kotlin/ru/tinkoff/fintech/tests/homework04/MyStackTest.kt
@@ -0,0 +1,44 @@
+package ru.tinkoff.fintech.tests.homework04
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import ru.tinkoff.fintech.homework.homework04.MyStack
+
+class MyStackTest {
+    private var stack = MyStack<Int>()
+
+    @BeforeEach
+    fun beforeEach() {
+        stack = MyStack()
+    }
+
+    @Test
+    fun testPopException() {
+        assertThrows<NoSuchElementException> {
+            stack.pop()
+        }
+    }
+
+    @Test
+    fun testPeekReturnsNull() {
+        assertNull(stack.peek())
+    }
+
+    @Test
+    fun testPop() {
+        stack.push(1).push(2).push(3)
+        val resultString = "" + stack.pop() + stack.pop() + stack.pop()
+        assertEquals("321", resultString)
+    }
+
+    @Test
+    fun testPeek() {
+        stack.push(1).push(2).push(3)
+        val resultString = "" + stack.peek() + stack.peek() + stack.peek() + stack.peek()
+        assertEquals("3333", resultString)
+    }
+
+}
