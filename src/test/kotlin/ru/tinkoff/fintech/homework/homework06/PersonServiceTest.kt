c952e0c68d66edeeb59004e214bceec1cc8d65dc
Dmitry Noskov <34381175+dmitrynoskov@users.noreply.github.com>
2022-03-29 23:42:26 +0300

    kotest running empty SpringBootTest problem solved

--- src/test/kotlin/ru/tinkoff/fintech/homework/homework06/PersonServiceTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/homework06/PersonServiceTest.kt
@@ -1,36 +1,23 @@
-package ru.tinkoff.fintech.tests.homework06
+package ru.tinkoff.fintech.homework.homework06
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.core.test.TestCase
 import io.kotest.core.test.TestResult
 import io.kotest.extensions.spring.SpringExtension
-import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
-import io.mockk.every
-import kotlinx.coroutines.delay
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultActionsDsl
-import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.post
-import kotlin.random.Random.Default.nextInt
-import kotlin.text.Charsets.UTF_8
-import ru.tinkoff.fintech.homework.homework06.service.PersonService
-import ru.tinkoff.fintech.homework.homework06.service.client.PersonInformationClient
 import ru.tinkoff.fintech.homework.homework06.Application
+import ru.tinkoff.fintech.homework.homework06.service.client.PersonInformationClient
+import kotlin.reflect.KClass
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PersonServiceTest(private val mockMvc: MockMvc, private val objectMapper: ObjectMapper): FeatureSpec() {
+class PersonServiceTest(private val mockMvc: MockMvc, private val objectMapper: ObjectMapper) : FeatureSpec() {
 
     @MockBean
     private lateinit var personInformationClient: PersonInformationClient
@@ -46,13 +33,12 @@ class PersonServiceTest(private val mockMvc: MockMvc, private val objectMapper: 
     }
 
     init {
-        feature("add person"){
+        feature("add person") {
             scenario("f") {
                 true
             }
         }
     }
-
 
 
 }
